a5963457e94321b9c17824ca8f2834f1a70eb033
diego moreno <diego.moreno@utp.edu.co>
2024-02-16 09:33:23 -0500

    Adding the dynamic rendering of the selected products

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -8,7 +8,19 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    # Caso en el que se este autenticado
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+            ) # Query para obtener el pedido del cliente
+        items = order.orderitem_set.all() # Query para obtener los items del pedido
+    # Dado que el usuario no esta autenticado
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
